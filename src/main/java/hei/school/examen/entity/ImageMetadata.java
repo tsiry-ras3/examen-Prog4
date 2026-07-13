74ef7e6b93727ebbc3ddfa20920e16979c3c01bc
Tsiry <tsiryras3@gmail.com>
2026-07-13 08:38:25 +0300

    feat: add entity and repository

---
+++ src/main/java/hei/school/examen/entity/ImageMetadata.java
@@ -0,0 +1,22 @@
+package hei.school.examen.entity;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ImageMetadata {
+    private UUID id;
+    private String nomFichier;
+    private String email;
+}
---
+++ src/main/java/hei/school/examen/repository/model/JImageMetadata.java
@@ -0,0 +1,29 @@
+package hei.school.examen.repository.model;
+
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "image_metadata")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class JImageMetadata {
+    @Id
+    private UUID id;
+    private String nomFichier;
+    private String email;
+}
